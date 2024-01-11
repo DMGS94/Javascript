747f6ef1077fe543f6cd143c65a5a82c40ba3675
David Soares <1994david.ds@gmail.com>
2024-01-11 02:45:38 +0000

    Update sellWeapon

--- Role_Playing_Game/script.js
+++ Role_Playing_Game/script.js
@@ -67,13 +67,22 @@ function buyHealth() {
     }
 }
 function buyWeapon() {
-    if (gold >= 30) {
-        gold -= 30;
-        currentWeapon++;
-        goldText.innerText = gold;
-        let newWeapon = weapons[currentWeapon].name;
-        goldText.innerText = "You now have a "+ newWeapon + ".";
-        inventory.push(newWeapon);
+    if (currentWeapon < weapons.length-1){
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            goldText.innerText = "You now have a "+ newWeapon + ".";
+            text.innerText += " In your inventory you have: " + inventory + ".";
+            inventory.push(newWeapon);
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
 }
 function update(location){
@@ -90,4 +99,7 @@ function fightSlime(){
 }
 function fightBeast(){
 
+}
+function sellWeapon(){
+    
 }
